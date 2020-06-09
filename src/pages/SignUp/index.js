b5066275cb2d0740f6a51ff73d3f56e107d33b80
luan jr <luan.silva@southsystem.com.br>
2020-06-09 13:52:18 -0300

    Adicionado estilização na pagina de login e no registro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -9,11 +9,12 @@ export default function SignIn() {
       <img src={logo} alt="Dragão" />
 
       <form>
+        <input type="text" name="name" placeholder="Seu nome" />
         <input type="email" name="email" placeholder="Seu e-mail" />
         <input type="password" name="password" placeholder="Sua senha" />
 
-        <button type="submit">Acessar</button>
-        <Link to="/register">Criar conta gratuita</Link>
+        <button type="submit">Criar Conta</button>
+        <Link to="/">Já tenho Login</Link>
       </form>
     </>
   );
