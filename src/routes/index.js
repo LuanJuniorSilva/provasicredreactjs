c4d6c713e0ede5cd96905d7e908bc20d89dd7852
luan jr <luan.silva@southsystem.com.br>
2020-06-09 11:06:10 -0300

    configurando rotas, deixando elas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import Dashboard from '../pages/Dashboard';
 
@@ -9,7 +10,9 @@ const Routes = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
 
-    <Route path="/dashboard" component={Dashboard} />
+    <Route path="/dashboard" component={Dashboard} isPrivate />
+
+    <Route path="/" component={() => <h1>404</h1>} />
   </Switch>
 );
 
