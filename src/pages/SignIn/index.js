8cb6b487c5fdc4b4b26a20cc14f80c636cf0a646
luan jr <luan.silva@southsystem.com.br>
2020-06-09 14:15:42 -0300

    Adicionando unform(para não precisar criar estado para cada campo do formulario) e validação nos campos do formulário

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '../../assets/logo.jpg';
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
+  function handleSubmit($data) {}
+
   return (
     <>
       <img src={logo} alt="Dragão" />
 
-      <form>
-        <input type="email" name="email" placeholder="Seu e-mail" />
-        <input type="password" name="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input type="password" name="password" placeholder="Sua senha" />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '../../assets/logo.jpg';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('O nome é obrigatório'),
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string()
+    .min(6, 'No mínimo 6 caracteres')
+    .required('A senha é obrigatória'),
+});
+
 export default function SignIn() {
+  function handleSubmit($data) {}
   return (
     <>
       <img src={logo} alt="Dragão" />
 
-      <form>
-        <input type="text" name="name" placeholder="Seu nome" />
-        <input type="email" name="email" placeholder="Seu e-mail" />
-        <input type="password" name="password" placeholder="Sua senha" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input type="text" name="name" placeholder="Seu nome" />
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input type="password" name="password" placeholder="Sua senha" />
 
         <button type="submit">Criar Conta</button>
         <Link to="/">Já tenho Login</Link>
-      </form>
+      </Form>
     </>
   );
 }
