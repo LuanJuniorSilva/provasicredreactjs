fde2e5ce8bcdbbb02ee5168fa8ebc291d43cd5db
luan jr <luan.silva@southsystem.com.br>
2020-06-11 17:54:37 -0300

    adicionando logout na aplicação

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { signOut } from '../../store/modules/auth/actions';
 
 // import { Container } from './styles';
 
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  const dispatch = useDispatch();
+
+  function habdleSignOut() {
+    dispatch(signOut());
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <button onClick={habdleSignOut}>SAIR</button>
+    </div>
+  );
 }
 
 export default Dashboard;
