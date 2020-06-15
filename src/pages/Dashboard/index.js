5b96946f435ee443cf8ece1a299a8814d13aaff1
luan jr <luan.silva@southsystem.com.br>
2020-06-15 17:12:41 -0300

    Ajustes na estilização do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,61 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 
+import { getDragonsRequest } from '../../store/modules/dragon/actions';
+
 function Dashboard() {
+  const dispatch = useDispatch();
+  const { dragons } = useSelector(state => state.dragon);
+
+  useEffect(() => {
+    dispatch(getDragonsRequest());
+  }, []);
+
+  console.log('dragons', dragons);
+
   return (
     <>
       <Header />
-      <div></div>
+      <section>
+        <Link to="/createDragon">CADASTRAR NOVO DRAGÃO</Link>
+
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Tipo</th>
+              <th>História</th>
+              <th>Opções</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dragons.map(dragon => (
+              <tr key={dragon.id}>
+                <td>{dragon.id}</td>
+                <td>{dragon.name}</td>
+                <td>{dragon.type}</td>
+                <td>{dragon.history}</td>
+                <td>
+                  <Link>
+                    <i class="far fa-list-alt"></i>
+                  </Link>
+                  <Link>
+                    <i class="fas fa-pen"></i>
+                  </Link>
+                  <Link>
+                    <i class="fas fa-trash-alt"></i>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
       <Footer />
     </>
   );
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,5 +1,83 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
-  background: #999;
+  background: #ddd;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    > a {
+      display: block;
+      background-color: #0000ff;
+      color: #fff;
+      padding: 20px;
+      border-radius: 5px;
+      margin-top: 20px;
+      margin-bottom: 20px;
+      transition: background 0.02s ease-in-out;
+      font-weight: 800;
+
+      &:hover {
+        background-color: ${darken(0.03, '#0000ff')};
+      }
+    }
+
+    table {
+      th {
+        background-color: #000;
+        color: #fff;
+        padding: 8px;
+      }
+
+      td {
+        text-align: center;
+      }
+
+      th:nth-child(1) {
+        width: 70px;
+      }
+
+      th:nth-child(2) {
+        width: 190px;
+      }
+
+      th:nth-child(3) {
+        width: 150px;
+      }
+
+      th:nth-child(4) {
+        width: 220px;
+      }
+
+      td {
+        padding: 15px;
+      }
+
+      td > a {
+        display: inline-block;
+        font-size: 18px;
+        margin-left: 10px;
+
+        &:active {
+          color: #000;
+        }
+      }
+
+      td > a:nth-child(1) {
+        color: ${darken(0.2, '#00ff00')};
+      }
+
+      td > a:nth-child(2) {
+        color: ${darken(0.1, '#0000ff')};
+      }
+
+      td > a:nth-child(3) {
+        color: ${darken(0.1, '#ff0000')};
+      }
+    }
+  }
 `;
