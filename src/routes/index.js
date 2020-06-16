f33adb2ac5da589adfd664767582e41170502f38
luan jr <luan.silva@southsystem.com.br>
2020-06-15 22:13:18 -0300

    Adicionando mais 3 rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
+import Dragon from '../pages/Dragon';
+import FormEdit from '../pages/FormEdit';
 
 const Routes = () => (
   <Switch>
@@ -13,6 +15,9 @@ const Routes = () => (
     <Route path="/register" component={SignUp} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/dragon/:id" component={Dragon} isPrivate />
+    <Route path="/formedit/" component={FormEdit} isPrivate />
+    <Route path="/formedit/:id" component={FormEdit} isPrivate />
 
     <Route path="/" component={() => <h1>404</h1>} />
   </Switch>
